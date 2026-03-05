91c46546fced19f178c697cfb2e779bb837b22cb
Nsem Confident <njocknsem@gmail.com>
2026-03-05 02:07:14 +0100

    bottom nav icon changes

--- src/navigation/AppTabs.tsx
+++ src/navigation/AppTabs.tsx
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '@/screens/HomeScreen';
 import PastQuestionsListScreen from '@/screens/PastQuestionsListScreen';
 import ProfileScreen from '@/screens/ProfileScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export type AppTabParamList = {
   Home: undefined;
@@ -13,7 +14,43 @@ export type AppTabParamList = {
 const Tab = createBottomTabNavigator<AppTabParamList>();
 
 const AppTabs = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
+  
+  <Tab.Navigator 
+  screenOptions={({ route }) => ({
+    headerShown: false,
+
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName: keyof typeof Ionicons.glyphMap;
+
+      if (route.name === 'Home') {
+        iconName = focused ? 'home' : 'home-outline';
+      } 
+      else if (route.name === 'PastQuestions') {
+        iconName = focused
+          ? 'document-text'
+          : 'document-text-outline';
+      } 
+      else {
+        iconName = focused ? 'person' : 'person-outline';
+      }
+
+      return (
+        <Ionicons name={iconName} size={size} color={color} />
+      );
+    },
+
+    tabBarActiveTintColor: '#167F71',
+    tabBarInactiveTintColor: 'gray',
+    tabBarStyle: {
+      height: 60,
+      paddingBottom: 8,
+    },
+    tabBarLabelStyle: {
+      fontSize: 12,
+      fontWeight: '600',
+    },
+  })}
+  >
     <Tab.Screen name="Home" component={HomeScreen} />
     <Tab.Screen name="PastQuestions" component={PastQuestionsListScreen} options={{ title: 'Past Questions' }} />
     <Tab.Screen name="Profile" component={ProfileScreen} />
